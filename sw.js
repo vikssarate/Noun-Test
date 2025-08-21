de42330723233923b37a361fc236d366a5a7739b
vikssarate <viksarate01@gmail.com>
2025-08-21 06:54:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,29 @@
-// sw for Exam+Memo (v1)
-const CACHE = 'exam-memo-v1';
-self.addEventListener('install', e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
-    './','./index.html','./sw.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
-  ])));
+// sw (v1) for exam practice demo
+const CACHE = 'exam-practice-v28'; // 🔹 Bump version to v10 so old cache is replaced
+
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll([
+            './',
+            './index.html',
+            './questions.json?v=img28', // 🔹 Also bump query param to force reload of JSON
+            './images/beam_ud1.png',
+            './images/Vu_m.png'
+        ]))
+    );
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
 });
